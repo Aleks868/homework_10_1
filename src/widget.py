41df8c64e6ffd14ff453c67d654b5102a88dbc1f
Aleks <lekc868@rambler.ru>
2024-09-30 16:08:01 +0500

    add def get_date

--- src/widget.py
+++ src/widget.py
@@ -1,24 +1,17 @@
 from src.masks import get_mask_account, get_mask_card_number
 
 
-def mask_accaunt_card(type_and_number: str) -> str:
+def mask_accaunt_card(user_card: str) -> None:
     """Функция маскирующая счет/карту"""
-    text_result = ""
-    digit_result = ""
-    digit_count = 0
-    for symbol in type_and_number:
-        if symbol.isalfa():
-            text_result += symbol
-        elif symbol.isdigit():
-            digit_count+=1
-            digit_result += symbol
-    if digit_count > 16:
-        return f"{text_result} {get_mask_account(digit_result)}"
+    if "Счет" in user_card:
+        mask_acc_number = f"{user_card[:4]} {get_mask_account(user_card[5:])}"
+        return print(mask_acc_number)
     else:
-        return f"{text_result} {get_mask_card_number(digit_result)}"
+        mask_card_num = f"{user_card[:-15]} {get_mask_card_number(user_card[-16:])}"
+        return print(mask_card_num)
 
 
-    print (mask_accaunt_card())
+
 
 
 def get_date(date_str: str) -> str:
