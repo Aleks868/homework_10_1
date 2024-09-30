789b10c513440dc21738d75bb07985d5239e7bf1
Aleks <lekc868@rambler.ru>
2024-09-30 15:22:55 +0500

    add def get_date

--- src/widget.py
+++ src/widget.py
@@ -16,3 +16,12 @@ def mask_accaunt_card(type_and_number: str) -> str:
         return f"{text_result} {get_mask_account(digit_result)}"
     else:
         return f"{text_result} {get_mask_card_number(digit_result)}"
+
+
+    print (mask_accaunt_card())
+
+
+def get_date(date_str: str) -> str:
+    "Функция изменения формата даты"
+    date_slice = date_str[0:10].split("-")
+    return ".".join(date_slice[::1])
